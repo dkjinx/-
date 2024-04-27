af1fc85abd04016e0970fed4de7ad7969f129c9d
dk <dk@wq.com>
2024-04-27 18:49:09 +0800

    improve login

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.AutoFillConstant;
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -11,6 +14,7 @@ import com.sky.vo.EmployeeLoginVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -73,6 +77,20 @@ public class EmployeeController {
     @PostMapping("/logout")
     @ApiOperation("登出接口")
     public Result<String> logout() {
+        log.info("员工登出");
+        return Result.success();
+    }
+
+    /**
+     * 新增
+     * @param employeeDTO
+     * @return
+     */
+    @PostMapping("/add")
+    @ApiOperation("新增员工")
+    public Result<String> addEmployee(@RequestBody EmployeeDTO employeeDTO){
+        log.info("新增员工:{}",employeeDTO);
+        employeeService.save(employeeDTO);
         return Result.success();
     }
 
