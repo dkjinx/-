6f0c22a76bde537d9cef9bd8bcf654aa44bba67f
dk <dk@wq.com>
2024-05-05 00:08:29 +0800

    employee pagequery

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -5,9 +5,11 @@ import com.sky.constant.JwtClaimsConstant;
 import com.sky.constant.MessageConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.handler.GlobalExceptionHandler;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -17,10 +19,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -93,6 +92,20 @@ public class EmployeeController {
         log.info("新增员工:{}",employeeDTO);
         employeeService.save(employeeDTO);
         return Result.success();
+    }
+
+    /**
+     * 分页查询
+     * @param employeepagequerydto
+     * @return
+     */
+    @GetMapping("/pagequery")
+    @ApiOperation("员工分页查询")
+    public Result<PageResult> pagequery(EmployeePageQueryDTO employeepagequerydto){
+        log.info("分页查询:");
+        PageResult pagequery = employeeService.pagequery(employeepagequerydto);
+        return Result.success(pagequery);
+
     }
 
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,5 +77,15 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateUser(currentId);
         employeeMapper.SaveEmployee(employee);
     }
+
+    @Override
+    public PageResult pagequery(EmployeePageQueryDTO employeepagequerydto) {
+        PageHelper.startPage(employeepagequerydto.getPage(), employeepagequerydto.getPageSize());
+        Page<Employee> page=employeeMapper.getByPageQuery(employeepagequerydto);
+
+
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
 
 }
